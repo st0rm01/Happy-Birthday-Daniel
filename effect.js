7fa33c174a473859d2f0f3bd48fb901f67282058
st0rm01 <happylife7786@gmail.com>
2025-07-21 00:23:44 +0100

    Update effect.js

--- effect.js
+++ effect.js
@@ -192,7 +192,7 @@ $('document').ready(function(){
 		function msgLoop(i) {
 			console.log("Showing message " + i + ": ", $("p:nth-child(" + i + ")").text()); // Debug log
 			if (i > 1) {
-				$("p:nth-child(" + (i-1) + ")").fadeOut('slow'); // Fade out previous without delay
+				$("p:nth-child(" + (i-1) + ")").fadeOut('slow'); // Fade out previous
 			}
 			$("p:nth-child(" + i + ")").fadeIn('slow').delay(1500).promise().done(function(){
 				if (i == 47) {
